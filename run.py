6251cb1ffe760550e2555912b3a0c531b44c2663
martinabeleda <martin.abeleda@gmail.com>
2021-03-18 13:55:31 +1100

    Add support for output files

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 from typing import Tuple
+from pathlib import Path
 
 import cv2
 
@@ -8,7 +9,7 @@ import cv2
 # Define some default arguments since the problem statement asks for the script to be runnable without command line
 # arguments
 VIDEO_PATH = "data/video_1.mp4"
-DISPLAY_RESOLUTION = (1920, 1080)
+FRAME_RATE = 20
 
 
 def get_input() -> bool:
@@ -25,7 +26,7 @@ def check_quit(input_key: str):
 def show_frame(args: argparse.Namespace, cap, subtractor):
     ret, frame = cap.read()
     if not ret:
-        return
+        sys.exit(0)
 
     if args.monochrome:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -35,20 +36,23 @@ def show_frame(args: argparse.Namespace, cap, subtractor):
         frame = cv2.bitwise_and(frame, frame, mask=forground_mask)
 
     cv2.imshow("video", frame)
+    return frame
 
 
 def set_frame_back(cap, num_frames: int = 1):
     current_frame = cap.get(cv2.CAP_PROP_POS_FRAMES) - 1
-    cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame - 1)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame - num_frames)
 
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--video-file-path", help="The filepath of the video to display", type=str, default=VIDEO_PATH)
-    parser.add_argument("--target-file-path", help="The filepath of the video to extract", type=str, default=None)
+    parser.add_argument("--video-file-path", help="The filepath of the video to display", type=Path, default=VIDEO_PATH)
+    parser.add_argument("--target-file-path", help="The filepath of the video to extract", type=Path, default=None)
+    parser.add_argument("--frame-rate", help="The target frame rate", type=int, default=FRAME_RATE)
+    parser.add_argument("--display-resolution", help="The resolution of the video", type=Tuple[int, int], default=None)
     parser.add_argument(
-        "--display-resolution", help="The resolution of the video", type=Tuple[int, int], default=DISPLAY_RESOLUTION
+        "--output-resolution", help="The resolution of the output file", type=Tuple[int, int], default=None
     )
     parser.add_argument("--monochrome", help="If set, play the video in monochrome", action="store_true")
     parser.add_argument(
@@ -64,11 +68,23 @@ def main():
     )
     args = parser.parse_args()
 
-    cap = cv2.VideoCapture(args.video_file_path)
+    cap = cv2.VideoCapture(str(args.video_file_path))
     subtractor = cv2.createBackgroundSubtractorMOG2()
 
+    if args.target_file_path is not None:
+        target_file_path = args.target_file_path
+    else:
+        target_file_path = args.video_file_path.resolve().parent / (str(args.video_file_path.stem) + "_processed.mp4")
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    output_resolution = args.output_resolution
+    if output_resolution is None:
+        output_resolution = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    out = cv2.VideoWriter(str(target_file_path), fourcc, args.frame_rate, output_resolution)
+
     while cap.isOpened():
-        show_frame(args, cap, subtractor)
+        frame = show_frame(args, cap, subtractor)
+        out.write(frame)
 
         input_key = get_input()
         check_quit(input_key)
@@ -89,6 +105,7 @@ def main():
                     break
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
 
 
