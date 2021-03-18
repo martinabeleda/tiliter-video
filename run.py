5af6f3b04acf0ecfae6f6aff2795e58604fca88a
martinabeleda <martin.abeleda@gmail.com>
2021-03-18 13:20:55 +1100

    Consolidate frame processing function

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 from typing import Tuple
 
@@ -10,19 +11,51 @@ VIDEO_PATH = "data/video_1.mp4"
 DISPLAY_RESOLUTION = (1920, 1080)
 
 
-def check_input(key: str) -> bool:
+def get_input() -> bool:
     """Checks for a keypress"""
-    return cv2.waitKey(100) & 0xFF == ord(key)
+    return cv2.waitKey(100) & 0xFF
+
+
+def check_quit(input_key: str):
+    if input_key == ord("q"):
+        print("Quitting")
+        sys.exit(0)
+
+
+def show_frame(args: argparse.Namespace, cap, subtractor):
+    ret, frame = cap.read()
+    if not ret:
+        return
+
+    if args.monochrome:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if args.segment:
+        forground_mask = subtractor.apply(frame)
+        frame = cv2.bitwise_and(frame, frame, mask=forground_mask)
+
+    cv2.imshow("video", frame)
+
+
+def set_frame_back(cap, num_frames: int = 1):
+    current_frame = cap.get(cv2.CAP_PROP_POS_FRAMES) - 1
+    cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame - 1)
 
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--video-file-path", help="The filepath of the video to display", type=str, default=VIDEO_PATH)
+    parser.add_argument("--target-file-path", help="The filepath of the video to extract", type=str, default=None)
     parser.add_argument(
         "--display-resolution", help="The resolution of the video", type=Tuple[int, int], default=DISPLAY_RESOLUTION
     )
     parser.add_argument("--monochrome", help="If set, play the video in monochrome", action="store_true")
+    parser.add_argument(
+        "--segment",
+        help="If set, run the segmentation algorithm and save the result to the target path",
+        action="store_true",
+    )
     parser.add_argument(
         "--log-level",
         help="Set the logger level",
@@ -32,32 +65,28 @@ def main():
     args = parser.parse_args()
 
     cap = cv2.VideoCapture(args.video_file_path)
+    subtractor = cv2.createBackgroundSubtractorMOG2()
 
     while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
+        show_frame(args, cap, subtractor)
 
-        cv2.imshow("video", cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) if args.monochrome else frame)
+        input_key = get_input()
+        check_quit(input_key)
 
-        if check_input("p"):
+        if input_key == ord("p"):
             print("Paused")
-            while True:
-                if check_input("b"):
-                    current_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-                    print(f"\tcurrent frame: {current_frame}")
-                    previous_frame = current_frame - 1
-                    print(f"\tsetting to previous frame: {previous_frame}")
-                    cap.set(cv2.CAP_PROP_POS_FRAMES, previous_frame)
-                    cv2.imshow("video", cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) if args.monochrome else frame)
 
-                if check_input("p"):
+            while True:
+                input_key = get_input()
+                check_quit(input_key)
+
+                if input_key == ord("b"):
+                    set_frame_back(cap)
+                    show_frame(args, cap, subtractor)
+
+                if input_key == ord("p"):
                     print("Unpaused")
                     break
-
-        if check_input("q"):
-            print("Quitting")
-            break
 
     cap.release()
     cv2.destroyAllWindows()
